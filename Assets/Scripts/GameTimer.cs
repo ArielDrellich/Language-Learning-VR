451de8bc18424c9bb3b2820804fe1c4577323f33
Sapir Hender <sapirhender@gmail.com>
2021-11-28 23:19:21 +0200

    Fix timer be matching to VR view, and continusly of the timer between scences

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class GameTimer : MonoBehaviour
 {
-	public float startTime;
-	public Text timerText;
+	private float startTime;
+	public TMPro.TMP_Text timerTextPro;
     // Start is called before the first frame update
     void Start()
     {
-        startTime = Time.time;
+    }
+
+    void OnEnable()
+    {
+        if (SceneManager.GetActiveScene().name.Equals("Living-Room-Scene")) {
+            startTime = Time.time;
+        } else {
+            startTime = PlayerPrefs.GetFloat("timerStartTime") - 1;
+        }
     }
 
     // Update is called once per frame
@@ -19,8 +29,17 @@ public class GameTimer : MonoBehaviour
 
     	float t = Time.time - startTime;
     	string minutes = ((int) t / 60).ToString();
-    	string seconds = (t % 60).ToString("f2");
-    	timerText.text = minutes + ":" + seconds;
+        float seconds_i = t % 60;
+    	string seconds = seconds_i.ToString("f2");
+        // string time_to_show;
+    	timerTextPro.text = minutes + ":" + seconds;
+
+        //Debug.Log(seconds);
+
+        if (Mathf.FloorToInt(seconds_i) == 10) {
+            // StaticClass.CrossSceneInfo.Set(time_to_show);
+            SceneManager.LoadScene("SampleScene");
+        }
     	// if (timeValue > 0) {
      //    	timeValue -= Time.deltaTime;
     	// } else
@@ -28,6 +47,11 @@ public class GameTimer : MonoBehaviour
     	// 	timeValue = 0;
     	// }
     	// DisplayTime(timeValue);
+    }
+
+    void OnDisable()
+    {
+        PlayerPrefs.SetFloat("timerStartTime", startTime);
     }
 
     // void DisplayTime(float TimeToDisplay)
