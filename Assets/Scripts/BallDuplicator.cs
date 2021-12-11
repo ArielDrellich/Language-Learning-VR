dce6b0fc413538e13c967918d760848bd1ef0385
Sapir Hender <sapirhender@gmail.com>
2021-12-11 21:20:37 +0200

    Add matching functionality (for item to text) and duplicate items

---
+++ Assets/Scripts/BallDuplicator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallDuplicator : MonoBehaviour
+{
+	private List<GameObject> clones;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clones = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	foreach (GameObject clone in clones) {
+    		Destroy(clone, 1);
+    	}
+    }
+
+    void OnCollisionEnter(Collision collision) {
+    	GameObject clone = Instantiate(collision.gameObject, new Vector3(10, 10, 10), Quaternion.identity);
+    	clones.Add(clone);
+    }
+}
---
+++ Assets/Scripts/MatchObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchObject : MonoBehaviour
+{
+	public string expectedTag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+    	if (collision.gameObject.tag == expectedTag) {
+    		Vector3 pos = collision.gameObject.transform.position + new Vector3(0, 5, 5);
+    		Debug.Log(pos);
+    		GameObject clone = Instantiate(collision.gameObject, pos, collision.gameObject.transform.rotation);
+
+    		// Destroy(clone);
+
+    		Debug.Log("Collision successfull");
+    	} else {
+    		Debug.Log("Collided with unexpected object " + collision.gameObject.tag);
+    	}
+    }
+}
