eff8560b499543f5cf05d69480f87335a4a50ccb
Sapir Hender <sapirhender@gmail.com>
2021-12-25 15:14:48 +0200

    Add initial translating functionality and creat font asset that support at the most of the languages

---
+++ Assets/Scripts/Translator.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System;
+using System.Web;
+using System.Net;
+
+public class Translator : MonoBehaviour
+{
+	public TMPro.TMP_Text wordToTranslate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Translate(wordToTranslate.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Translate(string word)
+    {
+            //Font font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+            var toLanguage = "sk";//Deutsch
+            var fromLanguage = "en";//English
+            var url = $"https://translate.googleapis.com/translate_a/single?client=gtx&sl={fromLanguage}&tl={toLanguage}&dt=t&q={HttpUtility.UrlEncode(word)}";
+            var webClient = new System.Net.WebClient
+            {
+                Encoding = System.Text.Encoding.UTF8
+            };
+            var result = webClient.DownloadString(url);
+            try
+            {
+                result = result.Substring(4, result.IndexOf("\"", 4, StringComparison.Ordinal) - 4);
+                wordToTranslate.text = result;
+                //return result;
+            }
+            catch
+            {
+                Debug.Log("Error");
+            }
+
+    }
+
+
+}
