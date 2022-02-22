b80e947ca042ed70e1ead9871d7e69320f021169
ArielDrellich <ArielDrellich@gmail.com>
2022-02-22 20:06:04 +0200

    AimClick implemented. Teleport, Main Menu, and Exit all work accordingly

---
+++ Assets/Scripts/ExitGameButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Menu;
+
+public class ExitGameButton : MonoBehaviour, IClickable
+{
+    ReticleManager reticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        reticle = GameObject.Find("Reticle").GetComponent<ReticleManager>();
+    }
+
+    public void LookedAt(RaycastHit hit) {
+        reticle.SetColor(Color.red);
+        if (Input.GetButtonDown("Fire1")) {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/Player Control/-TeleportPad.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _TeleportPad : MonoBehaviour
+{
+    Renderer teleportRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        teleportRenderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire2"))
+            teleportRenderer.enabled = true;
+        else 
+            teleportRenderer.enabled = false;
+    }
+}
