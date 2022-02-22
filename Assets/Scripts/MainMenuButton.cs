0f39d1758dc93a654d7a8bd4023aec02d22b82b3
ArielDrellich <ArielDrellich@gmail.com>
2022-02-22 21:02:52 +0200

    All except RecWord and PickUp changed to use IClickable
    No need for more raycasts, and IMenu and MenuClick deleted since they've been incorporated into AimClick

--- Assets/Scripts/MainMenuButton.cs
+++ Assets/Scripts/MainMenuButton.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Menu;
 using UnityEngine.SceneManagement;
 
 public class MainMenuButton : MonoBehaviour, IClickable
--- Assets/Scripts/RecWord.cs
+++ Assets/Scripts/RecWord.cs
@@ -2,61 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// public class RecWord : MonoBehaviour, IClickable
-// {
-// 	[SerializeField] private float charDistance = 10f;
-//     ReticleManager reticle;
-
-//     public GameObject submit;
-//     [SerializeField] private float submitDistance = 10f;
-//     private WordScramble ws;
-//     RaycastHit hit;
-
-//     void Start()
-//     {
-//         reticle = GameObject.Find("Reticle").GetComponent<ReticleManager>();
-//         ws = GameObject.Find("Core").GetComponent<WordScramble>();
-//     }
-
-//     public void LookedAt(RaycastHit hit) {
-//         // bool isChar = false;
-//         if (hit.distance <= charDistance) {
-//             // isChar = hit.collider.GetComponent<CharObject>();
-//             GameObject selected = hit.collider.gameObject;
-//             CharObject co = (CharObject)selected.GetComponent(typeof(CharObject));
-//             if (co != null) 
-//                 reticle.SetColor(Color.red);
-//             if (co != null && Input.GetButtonDown("Fire1")) {
-//             	co.Select();
-//             }
-//     	}
-
-//         Submit();
-//     }
-
-
-//     public void Submit()
-//     {
-//         bool didHitButton = Physics.Raycast(transform.position,
-//             transform.TransformDirection(Vector3.forward), out hit, submitDistance);
-
-//         if (didHitButton) {
-//             if (hit.collider.gameObject.name == submit.name) {
-//                 reticle.SetColor(Color.red);
-//                 if (Input.GetButtonDown("Fire1")) {
-//                     Debug.Log("Checking answer...");
-//                     if (ws.CheckWord()) {
-//                         Debug.Log("Success");
-//                     } else {
-//                         Debug.Log("You suck");
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }
-
-
 public class RecWord : MonoBehaviour
 {
 	RaycastHit hit;
