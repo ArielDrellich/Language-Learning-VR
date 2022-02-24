b1a052516c0b1f1eca65afd34e8e95609b5c9dcf
ArielDrellich <ArielDrellich@gmail.com>
2022-02-24 22:18:13 +0200

    Button Prefab created

---
+++ Assets/PressButtonTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressButtonTest : MonoBehaviour, IClickable
+{
+    ReticleManager reticle;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        reticle = GameObject.Find("Reticle").GetComponent<ReticleManager>();
+        animator = this.GetComponent<Animator>();
+    }
+
+    public void LookedAt(RaycastHit hit) {
+        reticle.SetColor(Color.red);
+        if (Input.GetButtonDown("Fire1")) {
+            animator.Play("button press");
+        }
+    }
+}
