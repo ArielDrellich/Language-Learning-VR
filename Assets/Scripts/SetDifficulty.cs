9c16c8c8d7f9973a4f86fb10f48c875a495e1512
Sapir Hender <sapirhender@gmail.com>
2022-04-18 22:02:50 +0300

    Sapir: Add difficulty buttons + tutorial button

---
+++ Assets/Scripts/SetDifficulty.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetDifficulty : MonoBehaviour, IClickable
+{
+    ReticleManager reticle;
+    void Start()
+    {
+        reticle = GameObject.Find("Reticle").GetComponent<ReticleManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LookedAt(RaycastHit hit)
+    {
+        reticle.SetColor(Color.red);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            string difficultyName = hit.transform.GetChild(0).name; // LanguageXtext
+            TMPro.TMP_Text difficultyTxt = GameObject.Find(difficultyName).GetComponent<TMPro.TMP_Text>();
+            PlayerPrefs.SetString("difficultyChoice", difficultyTxt.text);
+            switch (difficultyTxt.text)
+            {
+                case "easy":
+                    Debug.Log("easy");
+                break;
+                case "hard":
+                    Debug.Log("hard");
+                break;
+                case "medium":
+                    Debug.Log("medium");
+                break;
+                default:
+                    Debug.Log("default");
+                    break;
+            }
+        }
+    }
+}
+
+
