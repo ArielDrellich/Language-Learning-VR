7bb83dfad1933f756e117366dc51dfebd66b4ba5
Sapir Hender <sapirhender@gmail.com>
2022-05-03 23:44:56 +0300

    Sapir: Add internet check script

---
+++ Assets/Scripts/InternetCheck.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InternetCheck : MonoBehaviour
+{
+    TMPro.TMP_Text NoConnectionTxt;
+    PickUpV2 PickUp;
+    AimClick click;
+    PlayerMovement movement;
+    void Start()
+    {
+        NoConnectionTxt = GameObject.Find("NoConnectionTxt").GetComponent<TMPro.TMP_Text>();
+        NoConnectionTxt.enabled = false;
+        PickUp = GameObject.Find("Aim Set").GetComponent<PickUpV2>();
+        PickUp.enabled = true;
+        click = GameObject.Find("Aim Set").GetComponent<AimClick>();
+        click.enabled = true;
+        // if we are in the start scene, the player movement already enabled
+        Scene scene = SceneManager.GetActiveScene();
+        movement = GameObject.Find("Player").GetComponent<PlayerMovement>();
+        if (scene.name != "Start Menu" && scene.name != "Game Over Screen" && scene.name != "Win Screen")
+        {
+            movement.enabled = true;
+        }
+        Debug.Log(movement.enabled);
+
+
+    }
+
+    void Update()
+    {
+        if (Application.internetReachability == NetworkReachability.NotReachable)
+        {
+            NoConnectionTxt.enabled = true;
+            PickUp.enabled = false;
+            click.enabled = false;
+            movement.enabled = false;
+        }
+    }
+}
