9333291c815b6a765a0b0c1c17504934884f10ac
Sapir Hender <sapirhender@gmail.com>
2022-05-07 22:48:35 +0300

    Sapir: add colorfull script for title in the start scene

---
+++ Assets/Scripts/ColLet.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColLet : MonoBehaviour
+{
+    TMPro.TMP_Text gameName;
+    float startTime;
+
+    void Start()
+    {
+        gameName = GameObject.Find("LanguageLearningVR").GetComponent<TMPro.TMP_Text>();
+        gameName.text = RandomMenuColor();
+        startTime = Time.fixedTime;
+    }
+    void Update()
+    {
+        ChangeText();
+    }
+    string ColorToHex(Color32 color)
+    {
+        string hex = color.r.ToString("X2") + color.g.ToString("X2") + color.b.ToString("X2");
+        return hex;
+    }
+
+    Color32 RandomColor()
+    {
+        return new Color32((byte)UnityEngine.Random.Range(0, 255), (byte)UnityEngine.Random.Range(0, 255), (byte)UnityEngine.Random.Range(0, 255), 255);
+    }
+
+    string RandomMenuColor()
+    {
+        const string gameName = "Language Learning VR";
+        string coloredgameName = "";
+        foreach (char c in gameName)
+        {
+            coloredgameName += "<color=#" + ColorToHex(RandomColor()) + ">" + c + "</color>";
+
+        }
+        return coloredgameName;
+    }
+    void ChangeText()
+    {
+        if (Time.fixedTime - startTime > 2)
+        {
+            gameName.text = RandomMenuColor();
+            startTime = Time.fixedTime;
+        }
+    }
+}
