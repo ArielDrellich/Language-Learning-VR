0c0c5165c7ad73cefe67843da0d2b504f86aac61
Sapir Hender <sapirhender@gmail.com>
2022-05-07 22:47:57 +0300

    Sapir: Add sounds for win scene and tutorial scene

--- Assets/Scripts/Actions/PlaySound.cs
+++ Assets/Scripts/Actions/PlaySound.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Music for tutorial: Royalty Free Music from https://timtaj.com
+
 public class PlaySound : MonoBehaviour, IAction {
 
     public AudioSource audioSource = null;
