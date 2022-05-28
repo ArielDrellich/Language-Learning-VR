c49ce22c6f7f9a0477045dad9dcdc38d2cd04cdd
ArielDrellich <ArielDrellich@gmail.com>
2022-05-28 22:00:25 +0300

    Faded Start button when it can't be clicked

--- Assets/Scripts/Clickables/StartClick.cs
+++ Assets/Scripts/Clickables/StartClick.cs
@@ -7,13 +7,17 @@ public class StartClick : MonoBehaviour, IClickable
     ReticleManager reticle;
     GameObject     chooseLanguage;
     GameObject     chooseDifficulty;
-    bool           canStart = true;
+    MeshRenderer   rend;
+    bool           canStart         = false;
+    bool           languagePicked   = false;
+    bool           difficultyPicked = false;
 
     void Start()
     {
         reticle = GameObject.Find("Reticle").GetComponent<ReticleManager>();
         chooseLanguage = GameObject.Find("LanguageAlarm");
         chooseDifficulty = GameObject.Find("DifficultyAlarm");
+        rend = GetComponent<MeshRenderer>();
         chooseLanguage.SetActive(false);
         chooseDifficulty.SetActive(false);
     }
@@ -23,32 +27,58 @@ public class StartClick : MonoBehaviour, IClickable
         reticle.SetColor(Color.red);
         if (Input.GetButtonDown("Fire1"))
         {
-            if (PlayerPrefs.GetString("languageChoice").Equals(""))
-            {
-                StartCoroutine(ChooseLangAlarmCoroutine());
-                canStart = false;
-            }
-
-            if (PlayerPrefs.GetString("Difficulty").Equals(""))
-            {
-                StartCoroutine(ChooseDifAlarmCoroutine());
-                canStart = false;
-            }
-
             if (canStart)
             {
                 FindObjectOfType<LevelManager>().NextLevel();
             }
+            else
+            {
+                if (!languagePicked)
+                {
+                    StartCoroutine(ChooseLangAlarmCoroutine());
+                }
 
-            canStart = true;
+                if (!difficultyPicked)
+                {
+                    StartCoroutine(ChooseDifAlarmCoroutine());
+                }
+            }
         }
+    }
+
+    void Update()
+    {
+        languagePicked = PlayerPrefs.GetString("languageChoice").Equals("") ? false : true;
+
+        difficultyPicked = PlayerPrefs.GetString("Difficulty").Equals("") ? false : true;
+
+
+        if (languagePicked && difficultyPicked)
+        {
+            canStart = true;
+            rend.material.color = new Color(1f,1f,1f,1f);
+        }
+        else
+        {
+            canStart = false;
+            rend.material.color = new Color(1f,1f,1f,0.4f);
+        }
+
+        // if (canStart)
+        // {
+        //     rend.material.color = new Color(1f,1f,1f,1f);
+        // }
+        // else
+        // {
+        //     rend.material.color = new Color(1f,1f,1f,0.4f);
+        // }
     }
 
     IEnumerator ChooseLangAlarmCoroutine()
     {
-            chooseLanguage.SetActive(true);
-            yield return new WaitForSeconds(1);
-            chooseLanguage.SetActive(false);
+        chooseLanguage.SetActive(true);
+        yield return new WaitForSeconds(1);
+        chooseLanguage.SetActive(false);
     }
 
     IEnumerator ChooseDifAlarmCoroutine()
